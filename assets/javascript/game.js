9184a4681a77ec73e33edf576f1008c78f6def36
Josh F <fowlerjosh93@gmail.com>
2019-09-04 17:32:53 -0400

    fixed start game function to push text to HTML

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,17 +2,22 @@ $(document).ready(function () {
 
     // Declare Variables
     var randomNumber= "";
-    var userNumber= 0;
+    var userNumber= "";
     var wins= 0;
     var losses= 0;
     var isNumberEqual= false;
 
     function startGame() {
-        document.getElementById("ransom").innerHTML = randomNumber= Math.floor(Math.random() * (120-19) +19);
-        userNumber= 0;
-        wins= "";
-        losses= "";
-        var isNumberEqual= false;
+        randomNumber= Math.floor(Math.random() * (120-19) +19);
+        userNumber= "0";
+        wins= 0;
+        losses= 0;
+        isNumberEqual= false;
+        $("#ransom").html(randomNumber);
+        $("#gathered").html(userNumber);
+        $("#wins").html("Triumphs Over Evil: " + wins);
+        $("#losses").html("OOPS: " + losses);
+        
         
     }
 startGame();
