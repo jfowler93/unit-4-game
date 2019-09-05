7563093fff6dfc5e8ffaa65792d2ba4be0e3438f
Josh F <fowlerjosh93@gmail.com>
2019-09-05 12:31:49 -0400

    created function to randomize rupee value

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,13 +3,21 @@ $(document).ready(function () {
     // Declare Variables
     var randomNumber= "";
     var userNumber= "";
+    var rupeeOne= "";
+    var rupeeTwo= "";
+    var rupeeThree= "";
+    var rupeeFour= "";
     var wins= 0;
     var losses= 0;
     var isNumberEqual= false;
 
     function startGame() {
         randomNumber= Math.floor(Math.random() * (120-19) +19);
-        userNumber= "0";
+        rupeeOne= Math.floor(Math.random() * (12-1) +12);
+        rupeeTwo= Math.floor(Math.random() * (12-1) +12);
+        rupeeThree= Math.floor(Math.random() * (12-1) +12);
+        rupeeFour= Math.floor(Math.random() * (12-1) +12);
+        userNumber= 0;
         wins= 0;
         losses= 0;
         isNumberEqual= false;
@@ -19,9 +27,27 @@ $(document).ready(function () {
         $("#losses").html("OOPS: " + losses);
         
         
+        function rupeeValue() {
+            $("#rupee-1") = (rupeeOne);
+            $("#rupee-2") = (rupeeTwo);
+            $("#rupee-3") = (rupeeThree);
+            $("#rupee-4") = (rupeeFour);
+
+        };
+
+        rupeeValue();
     }
 startGame();
 
+// $(".btn").on("click" , function(){
+//     if (isNumberEqual) {
+//         return false;
+//     };
+    
+// $("#gathered").html(buttonValue);
+    
+
+});
 
 
 
@@ -36,6 +62,3 @@ startGame();
 
 
 
-
-
-})
